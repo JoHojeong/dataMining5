fe666815888fece1954f021a854a7ba4c74c34e1
DESKTOP-FAI9CGK\jo <1_dll@naver.com>
2021-12-19 09:48:15 +0900

    테스트데이터로 예측하고, 예측결과를 혼동행렬로 출력하는 과정

--- dataMiningProject.R
+++ dataMiningProject.R
@@ -45,3 +45,31 @@ s = svm(admit~., data = ucla_train)
 
 # 3-6. SVM(polynomial) 모델
 s = svm(admit~., data = ucla_train, kernel = 'polynomial')
+
+# 4. 테스트데이터로 예측하고, 예측결과를 혼동행렬로 출력하는 과정
+
+# 4-1. 결정트리 테스트 데이터 예측
+newd = data.frame(gre=ucla_test$gre, gpa=ucla_test$gpa, rank=ucla_test$rank)
+predict(r, newdata = newd)
+r_pred = predict(r, newd, type = 'class')
+confusionMatrix(r_pred, ucla_test$admit)
+
+# 4-2. 랜덤포레스트(트리 개수 50개) 테스트 데이터 예측
+small_forest_p = predict(small_forest, newdata=ucla_test)
+table(small_forest_p, ucla_test$admit)
+
+# 4-3. 랜덤포레스트(트리 개수 1000개) 테스트 데이터 예측
+large_forest_p = predict(large_forest, newdata=ucla_test)
+table(large_forest_p, ucla_test$admit)
+
+# 4-4. K-NN 테스트 데이터 예측
+k_p = knn(ucla_train, ucla_test, ucla_train$admit, k = 5)
+table(k_p, ucla_test$admit)
+
+# 4-5. SVM(radial basis) 테스트 데이터 예측
+svm_r_p = predict(s, newdata = ucla_test)
+table(svm_r_p, ucla_test$admit)
+
+# 4-6. SVM(polynomial) 테스트 데이터 예측
+svm_p_p = predict(s, newdata =  ucla_test)
+table(svm_p_p, ucla_test$admit)
